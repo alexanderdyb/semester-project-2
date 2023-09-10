ecda2813fbdf433c7302427e5a07d2cde67e5f1f
Alexander Dyb <104794629+alexanderdyb@users.noreply.github.com>
2023-09-10 22:15:00 +0200

    Search and filter functionality

---
+++ src/js/handlers/search.mjs
@@ -0,0 +1,10 @@
+// handlers/searchHandler.mjs
+
+export function searchListings(data, query) {
+  return data.filter((element) => {
+    if (query) {
+      return element.title.toLowerCase().includes(query.toLowerCase());
+    }
+    return true;
+  });
+}
--- src/js/ui/templates/listings.mjs
+++ src/js/ui/templates/listings.mjs
@@ -1,17 +1,53 @@
 import { getListings } from "../../api/listings/read.mjs";
 import { displayError } from "../../handlers/userFeedback/error.mjs";
 import { formatDate } from "../../handlers/conversion/date.mjs";
+import { searchListings } from "../../handlers/search.mjs";
+
 const listingsContainer = document.querySelector("#listingsContainer");
 
-export async function latestListings() {
+export function latestListings() {
+  setupSearchEventListener();
+  setupSortOrderEventListener();
+  latestListingsFunction();
+}
+
+function setupSearchEventListener() {
+  const searchBar = document.querySelector("#searchBar");
+  if (searchBar) {
+    searchBar.addEventListener("input", (event) => {
+      const query = event.target.value;
+      const sortOrder = document.querySelector("#sortOrder").value;
+      latestListingsFunction(query, sortOrder);
+    });
+  }
+}
+
+function setupSortOrderEventListener() {
+  const sortOrder = document.querySelector("#sortOrder");
+  if (sortOrder) {
+    sortOrder.addEventListener("change", (event) => {
+      const query = document.querySelector("#searchBar").value;
+      latestListingsFunction(query, event.target.value);
+    });
+  }
+}
+
+async function latestListingsFunction(query = "", sortOrder = "newest") {
   try {
     const data = await getListings();
     console.log(data);
 
     listingsContainer.innerHTML = "";
-    data.sort((a, b) => new Date(b.created) - new Date(a.created));
 
-    data.forEach((element) => {
+    if (sortOrder === "newest") {
+      data.sort((a, b) => new Date(b.created) - new Date(a.created));
+    } else if (sortOrder === "oldest") {
+      data.sort((a, b) => new Date(a.created) - new Date(b.created));
+    }
+
+    const filteredData = searchListings(data, query);
+
+    filteredData.forEach((element) => {
       const title = element.title;
       const image = element.media[0];
       const bids = element._count.bids;
@@ -19,21 +55,21 @@ export async function latestListings() {
       const id = element.id;
 
       listingsContainer.innerHTML += `
-            <div>
-              <div class="h-80">
-                <img src=${image} alt=${title} class="object-cover w-full h-full" />
-              </div>
-              <div class="bg-white p-5">
-                <div class="mb-5">
-                  <h3>${title}</h3>
-                  <p>Posted ${createdDate}</p>
-                  <p>${bids} bids</p>
-                </div>
-                <div class="border text-center py-2">
-                  <a href="/listing/?id=${id}" class="inline-block w-full h-100">Details</a>
-                </div>
-              </div>
-            </div>`;
+        <div>
+          <div class="h-80">
+            <img src=${image} alt=${title} class="object-cover w-full h-full" />
+          </div>
+          <div class="bg-white p-5">
+            <div class="mb-5">
+              <h3>${title}</h3>
+              <p>Posted ${createdDate}</p>
+              <p>${bids} bids</p>
+            </div>
+            <div class="border text-center py-2">
+              <a href="/listing/?id=${id}" class="inline-block w-full h-100">Details</a>
+            </div>
+          </div>
+        </div>`;
     });
   } catch (error) {
     listingsContainer.innerHTML = "";
