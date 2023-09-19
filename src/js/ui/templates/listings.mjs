4b3fc6ebb483ad4919b96258a332b3876a769276
Alexander Dyb <104794629+alexanderdyb@users.noreply.github.com>
2023-09-19 23:38:07 +0200

    Fixes

--- src/js/ui/templates/listings.mjs
+++ src/js/ui/templates/listings.mjs
@@ -10,24 +10,58 @@ import {
 
 let currentOffset = 0;
 const LIMIT = 10;
+let allData = [];
+let filteredData = [];
 
 const listingsContainer = document.querySelector("#listingsContainer");
+const loadMoreButton = document.querySelector("#loadMoreButton");
 
+/**
+ * Entry point to initialize and set up event listeners for the latest listings page.
+ *
+ * @function
+ * @export
+ */
 export function latestListings() {
   setupSearchEventListener(latestListingsFunction);
   setupSortOrderEventListener(latestListingsFunction);
-  setupLoadMoreButton(latestListingsFunction);
+  setupLoadMoreButton(loadMoreListings);
   latestListingsFunction("", "newest", true);
 }
 
 /**
- * Retrieves and displays the latest listings with optional query and sorting options.
+ * Fetches all listings from the server in batches until no more data is returned.
  *
  * @async
  * @function
- * @param {string} [query=""] - The search query.
- * @param {string} [sortOrder="newest"] - The sorting order (e.g., "newest" or "oldest").
- * @param {boolean} [resetOffset=false] - Whether to reset the offset.
+ * @param {number} [offset=0] - The offset to start retrieving listings from (default is 0).
+ * @param {number} [limit=10] - The maximum number of listings to retrieve per batch (default is 10).
+ * @returns {Promise<Object[]>} - A promise that resolves to an array containing all the listings.
+ */
+async function fetchAllListings(offset = 0, limit = 10) {
+  let data = [];
+  try {
+    while (true) {
+      const newData = await getListings(limit, offset);
+      if (newData.length === 0) break;
+      data = [...data, ...newData];
+      offset += limit;
+    }
+  } catch (error) {
+    console.error("Error fetching all listings:", error);
+    throw error;
+  }
+  return data;
+}
+
+/**
+ * Main function to retrieve, sort, filter, and display the latest listings based on the specified parameters.
+ *
+ * @async
+ * @function
+ * @param {string} [query=""] - The search query to filter the listings (default is an empty string).
+ * @param {string} [sortOrder="newest"] - The sorting order, can be "newest" or "oldest" (default is "newest").
+ * @param {boolean} [resetOffset=false] - Whether to reset the offset (default is false).
  */
 async function latestListingsFunction(
   query = "",
@@ -38,62 +72,90 @@ async function latestListingsFunction(
     currentOffset = 0;
     listingsContainer.innerHTML = "";
   }
+
   try {
-    const data = await getListings(LIMIT, currentOffset);
-
-    const loadMoreButton = document.querySelector("#loadMoreButton");
-    if (data.length > 0) {
-      currentOffset += data.length;
-
-      if (loadMoreButton) {
-        loadMoreButton.style.display = data.length < LIMIT ? "none" : "block";
-      }
-    } else {
-      if (loadMoreButton) {
-        loadMoreButton.style.display = "none";
-      }
-      return;
-    }
+    allData = await fetchAllListings();
 
     if (sortOrder === "newest") {
-      data.sort((a, b) => new Date(b.created) - new Date(a.created));
+      allData.sort((a, b) => new Date(b.created) - new Date(a.created));
     } else if (sortOrder === "oldest") {
-      data.sort((a, b) => new Date(a.created) - new Date(b.created));
+      allData.sort((a, b) => new Date(a.created) - new Date(b.created));
     }
 
-    const filteredData = searchListings(data, query);
+    filteredData = searchListings(allData, query);
 
-    filteredData.forEach((element) => {
-      const title = element.title ? element.title : "No title";
-      const image = element.media[0]
-        ? element.media[0]
-        : "../../src/assets/images/no-image.png";
-      const bids = element._count.bids;
-      const createdDate = formatDate(element.created);
-      const id = element.id;
-
-      listingsContainer.innerHTML += `
-        <div class="shadow-lg">
-          <div class="h-80">
-            <img src=${image} alt=${title} class="object-cover w-full h-full" />
-          </div>
-          <div class="bg-white p-5">
-            <div class="mb-5">
-              <h4>${title}</h4>
-              <p>Posted ${createdDate}</p>
-              <p>${bids} bids</p>
-            </div>
-            <div class="border text-center py-2 border-[#E8CC8B]">
-              <a href="/listing/?id=${id}" class="inline-block w-full h-100">Details</a>
-            </div>
-          </div>
-        </div>`;
-    });
+    displayListings();
   } catch (error) {
-    listingsContainer.innerHTML = "";
-    const errorMessageElement = document.querySelector(".errorMessage");
-    const customRegisterError = error;
-    displayError(error, errorMessageElement, customRegisterError);
-    errorMessageElement.classList.remove("hidden");
+    console.error("Entered catch block in latestListingsFunction:", error);
+    handleError(error);
   }
+}
+
+function displayListings() {
+  const dataToDisplay = filteredData.slice(
+    currentOffset,
+    currentOffset + LIMIT
+  );
+  dataToDisplay.forEach((element) => {
+    const title = element.title ? element.title : "No title";
+    const image = element.media[0]
+      ? element.media[0]
+      : "../../src/assets/images/no-image.png";
+    const bids = element._count.bids;
+    const createdDate = formatDate(element.created);
+    const id = element.id;
+
+    listingsContainer.innerHTML += `
+      <div class="shadow-lg">
+        <div class="h-80">
+          <img src=${image} alt=${title} class="object-cover w-full h-full" />
+        </div>
+        <div class="bg-white p-5">
+          <div class="mb-5">
+            <h4>${title}</h4>
+            <p>Posted ${createdDate}</p>
+            <p>${bids} bids</p>
+          </div>
+          <div class="border text-center py-2 border-[#E8CC8B]">
+            <a href="/listing/?id=${id}" class="inline-block w-full h-100">Details</a>
+          </div>
+        </div>
+      </div>`;
+  });
+
+  loadMoreButton.style.display =
+    currentOffset + LIMIT < filteredData.length ? "block" : "none";
+}
+
+/**
+ * Handles errors that occur during the fetching and displaying of listings.
+ * Displays an error message on the page.
+ *
+ * @function
+ * @param {Error} error - The error object containing the error details.
+ */
+function handleError(error) {
+  console.log("handleError function called");
+  listingsContainer.innerHTML = "";
+  let errorMessageElement = document.querySelector(".errorMessage");
+
+  if (!errorMessageElement) {
+    console.error("Error message element not found");
+    return;
+  }
+
+  const customRegisterError = error.message;
+  displayError(error, errorMessageElement, customRegisterError);
+  errorMessageElement.classList.remove("hidden");
+}
+
+/**
+ * Event handler function for the "Load More" button.
+ * Increases the offset and displays the next set of listings when the button is clicked.
+ *
+ * @function
+ */
+function loadMoreListings() {
+  currentOffset += LIMIT;
+  displayListings();
 }
